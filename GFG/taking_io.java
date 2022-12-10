0488aa4b203b3c44cce815854b96e56f137e9f1b
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2022-12-10 18:32:22 +0530

    In progress

--- GFG/taking_io.java
+++ GFG/taking_io.java
@@ -18,19 +18,21 @@ class Geeks{
 		int t = sc.nextInt();
 		
 		while(t-- > 0){
-		    int a = sc.nextInt();
-		    float f = sc.nextFloat();
-		    long l = sc.nextLong();
-		    byte b = sc.nextByte();
-		    
-		    String s = sc.nextLine();
 		    // Your code here
-		    System.out.println(a);
-		    System.out.println(f);
-		    System.out.println(l);
-		    System.out.println(b);
-		    System.out.println(s);
-		    
+		     int a = sc.nextInt();
+		     System.out.println(a);
+
+		     float b = sc.nextFloat();
+		     System.out.println(b); 
+
+		     long c = sc.nextLong();
+		     System.out.println(c);
+
+		     byte d= sc.nextByte();
+		     System.out.println(d);
+
+		     String s = sc.nextLine();
+		     System.out.println(s);
 		    
 		}
 		
